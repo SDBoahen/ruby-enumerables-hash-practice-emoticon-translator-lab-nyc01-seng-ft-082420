92c751c79c6edf43747776f595057287bf356a00
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-22 02:47:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
 # require modules here
 
-def load_library
+
+def 
+  
+  load_library
+  
+  
   # code goes here
+
 end
 
-def get_japanese_emoticon
+
+
+
+def 
+  
+  get_japanese_emoticon
+  
+  
   # code goes here
+
 end
 
-def get_english_meaning
+
+
+
+def 
+  
+  get_english_meaning
+  
+  
   # code goes here
+
 end
+
