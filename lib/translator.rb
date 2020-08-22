42391c8eca1f196d5e2dc81bc354b4689359831e
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-22 03:21:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,17 +1,32 @@
-# require modules here
-
 require "yaml"
 emoticons = YAML.load_file('emoticons.yml')
 
 
-
-
 def 
   
-  load_library
+  load_library(file)
   
   
-  # code goes here
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+                             puts emoticons.inspect
+
+  hash_slinger = emoticons.each_with_object({}) do
+               |(key, value), hash_out|
+
+    if !hash_out[key] 
+
+        hash_out[key] = 
+      {
+        :english  => value[0],
+        :japanese => value[1]
+      }
+
+    end
+
+    hash_slinger
+    # // lol xD //
+                                                end
+
 
 end
 
@@ -20,10 +35,21 @@ end
 
 def 
   
-  get_japanese_emoticon
+  get_japanese_emoticon(file,english_emoji)
+      puts
+      puts ":: #{japenese_emoji}"
+      puts
+  english_REF = load_library(file)
+
+      shrug = "idek what that means Fam  :T . . ."
   
-  
-  # code goes here
+  english_REF.each do |value, meaning|
+    if  english_REF[value][:english] == english_emoji
+        return english_REF[value][:japanese]       end
+                   end
+
+return shrug
+
 
 end
 
@@ -32,10 +58,21 @@ end
 
 def 
   
-  get_english_meaning
-  
-  
-  # code goes here
+  get_english_meaning(file, japenese_emoji)
+      puts
+      puts ":: #{japenese_emoji}"
+      puts
+  japanese_REF = load_library(file)
+
+      shrug = "idek what that means Fam  :T . . ."
+
+  japanese_REF.each do |value, meaning|
+    if  japanese_REF[value][:japanese] == japenese_emoji
+        return value                                 end
+                    end
+
+  return shrug
+
 
 end
 
